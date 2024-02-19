8e4973ff2af0219b4855ab10b7be5044be334438
Olena <ryb00@i.ua>
2024-02-19 13:44:25 +0000

    Add class PostList

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,10 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
@@ -27,4 +31,9 @@ class Comment(models.Model):
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
     
--- codestar/urls.py
+++ codestar/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import my_blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', my_blog, name='blog'),
+    path("", include("blog.urls"), name="blog-urls"),
     
 ]
 
